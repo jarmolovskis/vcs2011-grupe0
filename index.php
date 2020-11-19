e6df734b7f1a2817a95e414a7b2fc3b74c9baaae
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2020-11-19 20:23:30 +0200

    Paragraftas

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 
 <?php include "header.php" ?>
 
-
+<div>
 <h1>Cia yra pagrindinis puslapis</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima obcaecati ipsa omnis nulla vel necessitatibus quis laboriosam dignissimos qui non tempora, exercitationem rerum in explicabo voluptatum tenetur voluptatibus voluptate nemo.</p>
+<div>
 
 
 <?php include "footer.php" ?>
